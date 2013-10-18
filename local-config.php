3083cb32e8b7d62e1990304740b04fa98ac86d2f
Matt Banks <mjbanks@gmail.com>
2013-10-18 11:33:32 -0400

    turn on debug

--- local-config.php
+++ local-config.php
@@ -9,4 +9,10 @@ You may include other settings here that you only want enabled on your local dev
 define( 'DB_NAME', 'local_db_name' );
 define( 'DB_USER', 'local_db_user' );
 define( 'DB_PASSWORD', 'local_db_password' );
-define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+define( 'DB_HOST', 'localhost' );
+
+// ===========
+// Show errors
+// ===========
+ini_set( 'display_errors', 1 );
+define( 'WP_DEBUG_DISPLAY', true );
